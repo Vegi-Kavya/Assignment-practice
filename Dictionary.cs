2b85a79143b717debf01466fe604b6d0451d20ec
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-10-05 15:10:49 +0530

    Add files via upload

---
+++ Dictionary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> mydictionary = new Dictionary<string, string>();
+            //Add to insert elements
+            mydictionary.Add("Category", "Men Collections");
+            mydictionary.Add("Category1", "Women Collections");
+            mydictionary.Add("Product Name", "Wrist Watch");
+            mydictionary.Add("Product Name1", "Frock");
+            Console.WriteLine("The elements in dictionary are:");
+            foreach (KeyValuePair<string,string> dictionary in mydictionary)
+            {
+                Console.WriteLine("The elements in dictionary are:" + dictionary.Key + dictionary.Value);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
