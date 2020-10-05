2f2f4f01c192848228da11de39429e4bbed53c0a
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-10-05 15:16:13 +0530

    Add files via upload

---
+++ Queue1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue1
+{
+    class Queue1
+    {
+        static void Main(string[] args)
+        {
+            Queue<Int32> q = new Queue<Int32>();
+            q.Enqueue(2);
+            Console.WriteLine("The no.of elements in queue are:" + q.Count());
+            Console.WriteLine("The elements exist in queue is:" + q.Contains(2));
+            q.Enqueue(4);
+            Console.WriteLine("The no.of elements in queue are:" + q.Count());
+            Console.WriteLine("The elements exist in queue is:" + q.Contains(4));
+            q.Enqueue(6);
+            Console.WriteLine("The no.of elements in queue are:" + q.Count());
+            Console.WriteLine("The elements exist in queue is:" + q.Contains(6));
+            q.Enqueue(8);
+            Console.WriteLine("The no.of elements in queue are:" + q.Count());
+            Console.WriteLine("The elements exist in queue is:" + q.Contains(8));
+            q.Enqueue(10);
+            Console.WriteLine("The no.of elements in queue are:" + q.Count());
+            Console.WriteLine("The elements exist in queue is:" + q.Contains(10));
+            Console.WriteLine("The list of elements in queue are:");
+            foreach (int x in q)
+            {
+                Console.WriteLine("elements of queue are:" + x);
+            }
+            Console.WriteLine("pop element of queue is:" + q.Dequeue());
+            Console.WriteLine("The no.of elements in queue are:" + q.Count());
+            Console.WriteLine("The elements exist in queue is:" + q.Contains(2));
+            Console.WriteLine("pop element of queue is:" + q.Dequeue());
+            Console.WriteLine("The no.of elements in queue are:" + q.Count());
+            Console.WriteLine("The elements exist in queue is:" + q.Contains(4));
+            Console.WriteLine("pop element of stack is:" + q.Dequeue());
+            Console.WriteLine("The no.of elements in queue are:" + q.Count());
+            Console.WriteLine("The elements exist in queue is:" + q.Contains(6));
+            Console.WriteLine("pop element of stack is:" + q.Dequeue());
+            Console.WriteLine("The no.of elements in queue are:" + q.Count());
+            Console.WriteLine("The elements exist in queue is:" + q.Contains(8));
+            Console.WriteLine("pop element of queue is:" + q.Dequeue());
+            Console.WriteLine("The no.of elements in queue are:" + q.Count());
+            Console.WriteLine("The elements exist in queue is:" + q.Contains(10));
+            Console.WriteLine("The list of elements in queue are:");
+            foreach (int x in q)
+            {
+                Console.WriteLine("elements of queue are:" + x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stack2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack2
+{
+    class Stack2
+    {
+        static void Main(string[] args)
+        {
+            Stack<Int32> s = new Stack<Int32>();
+            s.Push(2);
+            Console.WriteLine("The no.of elements in stack are:" + s.Count());
+            Console.WriteLine("The elements exist in stack is:" + s.Contains(2));
+            s.Push(4);
+            Console.WriteLine("The no.of elements in stack are:" + s.Count());
+            Console.WriteLine("The elements exist in stack is:" + s.Contains(4));
+            s.Push(6);
+            Console.WriteLine("The no.of elements in stack are:" + s.Count());
+            Console.WriteLine("The elements exist in stack is:" + s.Contains(6));
+            s.Push(8);
+            Console.WriteLine("The no.of elements in stack are:" + s.Count());
+            Console.WriteLine("The elements exist in stack is:" + s.Contains(8));
+            s.Push(10);
+            Console.WriteLine("The no.of elements in stack are:" + s.Count());
+            Console.WriteLine("The elements exist in stack is:" + s.Contains(10));
+            Console.WriteLine("The elements in stack are:");
+            foreach (int x in s)
+            {
+                Console.WriteLine("elements of stack are:" + x);
+            }
+            Console.WriteLine("pop element of stack is:" + s.Pop());
+            Console.WriteLine("The no.of elements in stack are:" + s.Count());
+            Console.WriteLine("The elements exist in stack is:" + s.Contains(10));
+            Console.WriteLine("pop element of stack is:" + s.Pop());
+            Console.WriteLine("The no.of elements in stack are:" + s.Count());
+            Console.WriteLine("The elements exist in stack is:" + s.Contains(8));
+            Console.WriteLine("pop element of stack is:" + s.Pop());
+            Console.WriteLine("The no.of elements in stack are:" + s.Count());
+            Console.WriteLine("The elements exist in stack is:" + s.Contains(6));
+            Console.WriteLine("pop element of stack is:" + s.Pop());
+            Console.WriteLine("The no.of elements in stack are:" + s.Count());
+            Console.WriteLine("The elements exist in stack is:" + s.Contains(4));
+            Console.WriteLine("pop element of stack is:" + s.Pop());
+            Console.WriteLine("The no.of elements in stack are:" + s.Count());
+            Console.WriteLine("The elements exist in stack is:" + s.Contains(2));
+            Console.WriteLine("The elements in stack are:");
+            foreach (int x in s)
+            {
+                Console.WriteLine("elements of stack are:" + x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
