cd72876c3268dc69d59b1b1e81ce5d557e09fe75
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-10-06 18:19:39 +0530

    Add files via upload

---
+++ Regex.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace csharpRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //C# program that uses static Regex
+            // The input string again.
+            string input = "/content/alternate-1.aspx";
+
+            // This calls the static method specified.
+            Console.WriteLine(RegexUtil.MatchKey(input));
+            Console.ReadKey();
+        }
+    }
+    static class RegexUtil
+    {
+        static Regex _regex = new Regex(@"/content/([a-z0-9\-]+)\.aspx$");
+        /// <summary>
+        /// This returns the key that is matched within the input.
+        /// </summary>
+        static public string MatchKey(string input)
+        {
+            Match match = _regex.Match(input.ToLower());
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ RegularExpression1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression1
+{
+    class RegularExpression1
+    {
+        //C# program that uses Regex.IsMatch() method
+        // <summary>
+        // Test string using Regex.IsMatch static method.
+        // </summary>
+        static bool IsValid(string value)
+        {
+            return Regex.IsMatch(value, @"^[a-zA-Z0-9]*$");
+        }
+        static void Main(string[] args)
+        {
+            // Test the strings with the IsValid method.
+            Console.WriteLine(IsValid("dotnetperls0123"));
+            Console.WriteLine(IsValid("DotNetPerls"));
+            Console.WriteLine(IsValid(":-)"));
+            //Console.WriteLine(IsValid(null)); // Throws an exception
+            Console.WriteLine(IsValid("tomandjerryluckynumbersare4and1"));
+            Console.WriteLine(IsValid("i'm kavya"));
+
+
+            //C# program that uses RegexOptions.IgnoreCase
+            const string value = "TEST";
+            // ... This ignores the case of the "TE" characters.
+            if (Regex.IsMatch(value, "te..", RegexOptions.IgnoreCase))
+            {
+                Console.WriteLine(true);
+            }
+
+            const string value1 = "KAVYA";
+            // ... This ignores the case of the "TE" characters.
+            if (Regex.IsMatch(value1, "ka..", RegexOptions.IgnoreCase))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+            const string value2 = "KAVYA";
+            // ... This ignores the case of the "TE" characters.
+            if (Regex.IsMatch(value2, "vy..", RegexOptions.IgnoreCase))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
