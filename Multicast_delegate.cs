be0f791bf915a7f3e46532805e65945ae78ff3f5
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-10-07 15:37:47 +0530

    Add files via upload

---
+++ Multicast_delegate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multicast_delegate
+{
+    public delegate void MyDelegate(int a, int b);
+    //public delegate void mulDelegate(int a, int b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate sd = program1.sum;
+            MyDelegate md = program2.mul;
+            MyDelegate sm = sd + md;
+            sm(20,100);
+            MyDelegate ld = (int a, int b) => Console.WriteLine("Lambda Expression: {0}", a + b);
+            sm = sm + ld;
+            sm(20, 40);
+            sm -= md;
+            sm(30, 5);
+            sm -= sd;
+            sm(10, 10);
+            Console.ReadKey();
+        }
+    }
+    class program1
+    {
+        public static void sum(int a,int b)
+        {
+            Console.WriteLine("sum is: {0} ", a + b);
+        }
+    }
+    class program2
+    {
+        public static void mul(int a, int b)
+        {
+            Console.WriteLine("mul is: {0}", a * b);
+        }
+    }
+}
