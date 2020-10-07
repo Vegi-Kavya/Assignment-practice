fec54b01961c75c1a068e2e54ef22c6f1a10add7
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-10-07 15:01:50 +0530

    Add files via upload

---
+++ Delegate3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Passing Delegate as a Parameter
+namespace Delegate3
+{
+    public delegate void MyDelegate(string msg); //declaring a delegate
+
+    class Delegate3
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate del = ClassA.MethodA;
+            InvokeDelegate(del);
+
+            del = ClassB.MethodB;
+            InvokeDelegate(del);
+
+            del = (string msg) => Console.WriteLine("Called lambda expression: " + msg);
+            InvokeDelegate(del);
+            Console.ReadKey();
+        }
+        static void InvokeDelegate(MyDelegate del) // MyDelegate type parameter
+        {
+            del("Hello World");
+        }
+    }
+    class ClassA
+    {
+        public static void MethodA(string message)
+        {
+            Console.WriteLine("Called ClassA.MethodA() with parameter: " + message);
+        }
+    }
+
+    class ClassB
+    {
+        public static void MethodB(string message)
+        {
+            Console.WriteLine("Called ClassB.MethodB() with parameter: " + message);
+        }
+    }
+}
---
+++ Delegate4.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate4
+{
+    public delegate void Method1Delegate(string msg);
+    //public delegate void Method2Delegate(string msg);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method1Delegate md1 = new Method1Delegate(program1.method1);
+            InvokeDelegate(md1);
+            md1 = program2.method2;
+            InvokeDelegate(md1);
+            md1 = (string msg) => Console.WriteLine("lambda expression: "+msg);
+            InvokeDelegate(md1);
+            Console.ReadKey();
+        }
+        static void InvokeDelegate(Method1Delegate md1)
+        {
+                md1("invoke method");
+        }
+    }
+    class program1
+    {
+        public static void method1(string message)
+        {
+            Console.WriteLine("This is Method1"+message);
+        }
+    }
+    class program2
+    {
+        public static void method2(string message)
+        {
+            Console.WriteLine("This is Method2"+message);
+        }
+    }
+}
