9e0f8c02a9ce75bf2fb49f95c81f220d274033d8
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-10-07 16:00:57 +0530

    Add files via upload

---
+++ Muldelretnval.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multicastdelegateretnval
+{
+    public delegate int Mydelegate();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            delegate1 d = new delegate1();
+            Mydelegate md1 = new Mydelegate(d.sum);
+            Mydelegate md2 = new Mydelegate(d.mul);
+            Mydelegate md=md1+md2;
+            Console.WriteLine(md());
+            Console.ReadKey();
+        }
+    }
+    class delegate1
+    {
+        public int sum()
+        {
+            return 50;
+        }
+        public int mul()
+        {
+            return 150;
+        }
+    }
+}
