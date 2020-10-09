b1c30dd3dbaf3c5765b06520cf1712ed2692a108
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-10-09 14:52:44 +0530

    Add files via upload

---
+++ MultiThreading.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreading
+{
+    
+    class Program
+    {
+        public  string Thread1()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 2)
+                {
+                    Console.WriteLine("Thread1 is waiting for 2 seconds");
+                    Thread.Sleep(2000);
+                }
+                Console.WriteLine( "Thread1 is started at the position "+i);
+            }
+            return "Thread1 got exit";
+        }
+        public string Thread2()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    Console.WriteLine("Thread2 is waiting for 5 seconds");
+                    Thread.Sleep(5000);
+                }
+                Console.WriteLine( "Thread2 is started at the position "+i);
+            }
+
+            return "Thread2 got exit ";
+        }
+        public static string Thread3()
+        {
+            return "Thread3 is started";
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            Thread T1 = new Thread(delegate ()
+              {
+                  Console.WriteLine(obj.Thread1());
+  
+              });
+            T1.Start();
+            Thread T2 = new Thread(delegate ()
+            {
+                Console.WriteLine(obj.Thread2());
+
+            });
+            T2.Start();
+            Console.ReadKey();
+        }
+    
+    }
+}
