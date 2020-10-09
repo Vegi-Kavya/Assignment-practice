4c085bc39bd0144b96b10ed79be1ab6ebf539c1d
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-10-09 15:43:23 +0530

    Add files via upload

---
+++ SingleThreading.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SingleThreading
+{
+    class Program
+    {
+        public string Thread1()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    Console.WriteLine("Thread1 is waiting for 5 seconds");
+                    Thread.Sleep(5000);
+
+                }
+                Console.WriteLine("Thread1 is started at the position " + i);
+
+            }
+            return "Thread1 is got exit";
+        }
+        public string Thread2()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                
+                Console.WriteLine("Thread2 is started at the position " + i);
+
+            }
+
+            return "Thread2 is got exit ";
+        }
+        public static string Thread3()
+        {
+            return "Thread3 is started";
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Thread T = new Thread(delegate ()
+            {
+                Console.WriteLine(p.Thread1());
+            });
+            T.Start();
+            T.Join();
+            Thread T1 = new Thread(delegate ()
+            {
+                Console.WriteLine(p.Thread2());
+            });
+            T1.Start();
+            Console.ReadKey();
+        }
+    }
+}
