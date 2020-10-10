314b04b8f3e2573e2cd34cf2008688506980c650
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-10-10 13:51:21 +0530

    Add files via upload

---
+++ Join.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace JoininMultithreading
+{
+    class Program
+    {
+        static void Dxc()
+        {
+            Console.WriteLine("Dxc training will start");
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("Training is started " + i);
+            }
+            Console.WriteLine("Training  is ended");
+        }
+        static void Alchemy()
+        {
+            Console.WriteLine("Alchemy start training");
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("Training is going on " + i);
+            }
+            Console.WriteLine("Training is ended by trainer");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Thread started");
+            Thread T1 = new Thread(Dxc);//Bind the method with method object
+            Thread T2 = new Thread(Alchemy);
+            T1.Start();//run the method
+            T2.Start();
+            T1.Join();
+            T2.Join();
+            Console.WriteLine("Main Thread exiting");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Locks.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace LocksinMultiThreading
+{
+    class Locks
+    {
+        public void Dxc()
+        {
+            lock (this)//is used to lock other thread where the current thread is sleeping/long time to response
+            {
+                Console.Write("Dxc Provide Training on ");
+                Thread.Sleep(5000);
+                Console.WriteLine("Csharp programming");
+                Console.WriteLine();
+            }
+        }
+        public void Alchemy()
+        {
+            lock (this)
+            {
+                Console.Write("Alchemy Solutions giving training on ");
+                Console.WriteLine("Csharp programming");
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Locks obj = new Locks();
+            Thread t1 = new Thread(obj.Dxc);
+            Thread t2 = new Thread(obj.Alchemy);
+            Thread t3 = new Thread(obj.Dxc);
+            Thread t4 = new Thread(obj.Alchemy);
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            Console.ReadKey();
+        }
+    }
+}
