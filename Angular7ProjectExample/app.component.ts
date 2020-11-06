28f565757a4c57cc1ee80dbc812817e1a814f81b
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-11-06 19:48:02 +0530

    Add files via upload

---
+++ Angular7ProjectExample/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'Welcome to Angular7ProjectExample';
+    title1 = "I'm getting in visual studio code";
+    subtitle = '.NET Core + Angular CLI v7 + Bootstrap & FontAwesome + Swagger Template';
+}
