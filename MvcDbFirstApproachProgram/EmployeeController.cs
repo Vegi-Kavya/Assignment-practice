762ff5d33c4159d290b116bc920c4a0b17e13f9e
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-11-13 18:48:59 +0530

    Add files via upload

---
+++ MvcDbFirstApproachProgram/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDbFirstApproach.Models;
+
+namespace MvcDbFirstApproach.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDBEntities1 emp = new EmployeeDBEntities1();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult Index1()
+        {
+            return View(emp.Employees.ToList());
+        }
+    }
+}
