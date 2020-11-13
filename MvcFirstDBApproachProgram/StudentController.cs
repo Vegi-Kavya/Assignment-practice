cddbe538c1a81759f93a58ac42d56b8480e77395
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-11-13 18:24:24 +0530

    Add files via upload

---
+++ MvcFirstDBApproachProgram/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcFirstDBApproach.Models;
+
+namespace MvcFirstDBApproach.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentDBEntities db = new StudentDBEntities();
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult Index1()
+        {
+            return View(db.Students.ToList());
+        }
+    }
+}
