c1e6c5c440c106df21a8a8bb71f41bf082fe75f8
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-11-13 14:24:18 +0530

    Add files via upload

---
+++ MvcEntityEmployee/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCEntityEmployee.Models
+{
+    [Table("Employee1")]
+    public class Employee
+    {
+        [Key]
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public int Age { get; set; }
+        public string Department { get; set; }
+    }
+}
---
+++ MvcEntityEmployee/EmployeeContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVCEntityEmployee.Models
+{
+    public class EmployeeContext:DbContext
+    {
+        public EmployeeContext() : base("EmployeeConnection")//Creating Constructor
+        {
+
+        }
+        public DbSet<Employee> Employee1 { get; set; }
+    }
+}
