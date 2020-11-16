bc92b807755144e5d5e5d7e6e175565184b96574
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-11-16 17:38:23 +0530

    Add files via upload

---
+++ MvcAppWithDbProject/EmployeeRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using MyApp.Models;
+
+namespace MvcAppDb.DBOperation
+{
+    public class EmployeeRepository
+    {
+        public int AddEmployee(EmployeeModel model)
+        {
+            using(var context=new EmployeeDBEntities())
+            {
+                Employee emp = new Employee()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email,
+                    Code = model.Code
+                };
+                context.Employee.Add(emp);
+                context.SaveChanges();
+                return emp.id;
+            }
+        }
+    }
+}
