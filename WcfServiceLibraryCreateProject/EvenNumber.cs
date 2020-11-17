28251ae508fbac77114b65e87ef1e5ec68893c21
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-11-17 14:08:04 +0530

    Add files via upload

---
+++ WcfServiceLibraryCreateProject/EvenNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibraryCreate
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EvenNumber" in both code and config file together.
+    public class EvenNumber : IEvenNumber
+    {
+        public string Even(int num1)
+        {
+            if (num1 % 2 == 0)
+                return "Even Number";
+            else
+                return "Odd Number";
+        }
+    }
+}
---
+++ WcfServiceLibraryCreateProject/IEvenNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibraryCreate
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEvenNumber" in both code and config file together.
+    [ServiceContract]
+    public interface IEvenNumber
+    {
+        [OperationContract]
+        //void DoWork();
+        string Even(int num1);
+    }
+}
+
