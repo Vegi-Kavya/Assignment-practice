5d58a1796ad996040bbf8e6802f0261d808fa1bd
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-11-17 19:19:23 +0530

    Add files via upload

---
+++ WcfCreateServiceProgram/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfCreateService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Calculator" in both code and config file together.
+    //Menu to change the classname calculator
+    //In both code and config file together
+    public class Calculator : ICalculator
+    {
+        public int Sum(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+        public int Sub(int num1, int num2)
+        {
+            if (num1 > num2)
+                return num1 - num2;
+            else
+                return num2-num1;
+        }
+        public int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int Divide(int num1, int num2)
+        {
+            if (num2 != 0)
+                return num1 / num2;
+            else
+                return 0;
+        }
+    }
+}
---
+++ WcfCreateServiceProgram/ICalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfCreateService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICalculator" in both code and config file together.
+    //Menu to change interface name ICalculator in both code 
+    //and config file together
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract]
+        //void DoWork();
+        int Sum(int num1, int num2);
+        [OperationContract]
+        int Sub(int num1, int num2);
+        [OperationContract]
+        int Mul(int num1, int num2);
+        [OperationContract]
+        int Divide(int num1, int num2);
+    }
+}
+//use a data contract composite type to service operations
+[DataContract]//Declare and assign variable
+public class CompositeType
+{
+    bool boolvalue = true;//boolean value assigned true
+    string stringval = "Hello World";//string value returned hello world
+    [DataMember]//you call are invoke data information
+    public bool BoolValue
+    {
+        get { return boolvalue; }
+        set { boolvalue = value; }
+    }
+    public string StringValue
+    {
+        get { return stringval; }
+        set { stringval = value; }
+    }
+}
