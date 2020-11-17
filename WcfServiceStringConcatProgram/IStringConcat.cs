e62b6e66601c536e11700b7f92c98d03e3ca73ff
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-11-17 19:26:04 +0530

    Add files via upload

---
+++ WcfServiceStringConcatProgram/IStringConcat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceStringConcat
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IStringConcat" in both code and config file together.
+    [ServiceContract]
+    public interface IStringConcat
+    {
+        [OperationContract]
+        //void DoWork();
+        string Concat(string a, string b);
+    }
+}
