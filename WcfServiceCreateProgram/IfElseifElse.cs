1bb7c0244a4671cfd450ad3caf2e2753f3e65b3f
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-11-17 19:17:13 +0530

    Add files via upload

---
+++ WcfServiceCreateProgram/IfElseifElse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceCreate
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "IfElseifElse" in both code and config file together.
+    public class IfElseifElse : IIfElseifElse
+    {
+        public string Equal(int n)
+        {
+            if (n < 1)
+                return "n is less than 1";
+            else if (n == 1)
+                return "n is equal to 1";
+            else
+                return "n is greater than 1";
+        }
+    }
+}
