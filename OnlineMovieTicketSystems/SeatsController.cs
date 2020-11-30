59f7bd796b00e132303734df19460fe7a2448491
Vegi-Kavya <72140130+Vegi-Kavya@users.noreply.github.com>
2020-11-30 21:55:48 +0530

    Add files via upload

---
+++ OnlineMovieTicketSystems/SeatsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineMovieTicketSystem.Models;
+
+namespace OnlineMovieTicketSystem.Controllers
+{
+    public class SeatsController : Controller
+    {
+        SeatsEntities db = new SeatsEntities();
+        // GET: Seats
+        public ActionResult Seat_Menu()
+        {
+            return View();
+        }
+        public ActionResult Block_Seat()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Block_Seat(Seattbl seat)
+        {
+            TempData["BlockMessage"] = "<script>alert('Successfully blocked your seats')</script>";
+            return RedirectToAction("Payment", "Seats");
+        }
+        public ActionResult Book_Seat()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Book_Seat(Seattbl seat)
+        {
+            TempData["BookMessage"] = "<script>alert('Successfully booked your seats')</script>";
+            return RedirectToAction("Payment", "Seats");
+        }
+        public ActionResult Cancel_Seat()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Cancel_Seat(Seattbl seat)
+        {
+            TempData["CancelMessage"] = "<script>alert('Successfully Canceled your seats')</script>";
+            return RedirectToAction("Payment", "Seats");
+        }
+        public ActionResult Payment()
+        {
+            return View();
+        }
+    }
+}
---
+++ OnlineMovieTicketSystems/UserController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineMovieTicketSystem.Models;
+
+namespace OnlineMovieTicketSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        UserEntities db = new UserEntities();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View(db.Table_User.ToList());
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Table_User user)
+        {
+            if (db.Table_User.Any(x => x.UserName == user.UserName))
+            {
+                ViewBag.Notification = "This User is already existed";
+                return View();
+            }
+            else
+            {
+                TempData["RegisterMessage"] = "<script>alert('User Register successfully')</script>";
+                db.Table_User.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Signin", "User");
+            }
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Table_User user)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Table_User.Add(user);
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    TempData["InsertMessage"] = "<script>alert('Data Inserted successfully!!')</script>";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["InsertMessage"] = "<script>alert(' Not Inserted!!')</script>";
+                }
+            }
+            return View();
+        }
+        public ActionResult Signout()
+        {
+            TempData["SignoutMessage"] = "<script>alert('User Signout successfully')</script>";
+            Session.Clear();
+            return RedirectToAction("Register", "User");
+        }
+        [HttpGet]
+        public ActionResult Signin(string UserName, string Password)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signin(Table_User user)
+        {
+            var checklogin = db.Table_User.Where(x => x.UserName.Equals(user.UserName) && x.Password.Equals(user.Password)).FirstOrDefault();
+            if (checklogin != null)
+            {
+                Session["UId"] = user.UserId.ToString();
+                Session["UName"] = user.UserName.ToString();
+                TempData["SigninMessage"] = "<script>alert('User Signin successfully')</script>";
+                return RedirectToAction("Seat_Menu","Seats");
+            }
+            else
+            {
+                ViewBag.Notification = "Incorrect Username or Password";
+            }
+            
+            return View();
+        }
+        
+    }
+}
